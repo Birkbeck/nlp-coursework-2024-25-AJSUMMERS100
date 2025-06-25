6bf5ab5273193731431e2a61fd20c6ba5ec55469
AJSUMMERS100 <alex.summers42@icloud.com>
2025-06-25 16:45:41 +0100

    Completed Q2A(ii). Wasn't sure if the question wanted NA party values removed. After re-reading the question, it mentions how the speaker value needs to be removed. That would imply that as the speaker value would be the fourth top value(excluding NA's), that therefore NA rows also need to be removed.

--- PartTwo.py
+++ PartTwo.py
@@ -2,6 +2,8 @@ import nltk
 import spacy
 from pathlib import Path
 import pandas as pd
+from collections import Counter
+
 
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 2000000
@@ -9,7 +11,29 @@ nlp.max_length = 2000000
 def part_a(path):
     pre_df = pd.read_csv(path)
     df = pd.DataFrame(pre_df)
+
+    # Part I
     df.replace("Labour (Co-op)", "Labour", inplace = True)
+
+    # Part II
+    df = df[df['party'].notna()]
+    df = df[df['party']!='Speaker']
+    partys = Counter()
+    for each in df['party']:
+        partys[each] +=1
+    top_four = Counter(partys).most_common(4)
+    top_four_list=[]
+    for i in range(4):
+        top_four_list.append(top_four[i][0])
+    for each in partys:
+        if each not in top_four_list:
+            df = df[df['party']!=each]
+    new_counters = Counter()
+    for each in df['party']:
+        new_counters[each] +=1
+
+    # Part III
+    
     return df
 
 if __name__ == "__main__":
